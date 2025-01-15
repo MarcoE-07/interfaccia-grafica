5455f9cdeda1e408c50523d0b261a8a7115a6ec3
MarcoE-07 <marcoelia3507@gmail.com>
2025-01-15 10:06:10 +0100

    prima

---
+++ Applicazione.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Applicazione {
+   public static void main(String[] args){
+     F1 nuova =new F1("Esercitazione");
+     nuova.setVisible(true);
+     nuova.setLocation(300,400);
+     nuova.setSize(null);
+
+   }
+    
+ }
+ 
---
+++ MiaFinestra2.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MiaFinestra2 extends JFrame{
+        JPanel p1;
+        JPanel p2;
+        JPanel p3;
+        JPanel p4;
+        JTextField tf1;
+        JTextField tf2;
+        JLabel l1;
+        JLabel l2;
+        JButton b1;
+    public MiaFinestra2 (String titolo){
+        super(titolo);
+        //Istanzio i widgets
+        p1 = new JPanel(new GridLayout(2,0);
+        p2 = new JPanel(new GridLayout(2,0);
+        p3 = new JPanel(new BorderLayout());
+        p4 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        tf1 = new JTextField();
+        tf2 = new JTextField();
+        l1 = new JLabel("Lire");
+        l2 = new JLabel("Euro");
+        b1 = new JButton("Converti");
+        //Posizionamento
+        add()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+
+
+
+
+
+
